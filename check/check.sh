1a01470f6bc5f906bd705f5707d4113abc9fdd55
Adam Matheson <adam.matheson@ntlworld.com>
2018-03-18 17:49:37 +0000

    Added updated check & personal_log

---
+++ check/check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python check.py --ref-av-vels-file 128x128.av_vels.dat --ref-final-state-file 128x128.final_state.dat --av-vels-file ../av_vels.dat --final-state-file ../final_state.dat
